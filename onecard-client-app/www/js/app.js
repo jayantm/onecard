6a72ef230fd722eee9cb4529684ee930959b189d
OnTechEdge <ImTechyCode@gmail.com>
2015-09-28 12:08:43 +0530

    OAuth for google and facebook
    Added logic for login for google and facebook login

--- onecard-client-app/www/js/app.js
+++ onecard-client-app/www/js/app.js
@@ -3,95 +3,62 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+var starterController = angular.module('starter', ['ionic','ngCordova'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
-
     }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleLightContent();
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
     }
   });
-})
+});
 
-.config(function($stateProvider, $urlRouterProvider) {
-
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
-
-  .state('start', {
-    url: '/start',
-    templateUrl: 'templates/index.html',
-    //controller: 'LoginCtrl'
-  })
-
-  .state('login', {
-    url: '/login',
-    templateUrl: 'templates/login.html',
-    //controller: 'LoginCtrl'
-  })
-
-  // setup an abstract state for the tabs directive
-  .state('tab', {
-    url: '/tab',
-    abstract: true,
-    templateUrl: 'templates/tabs.html'
-  })
-
-  // Each tab has its own nav history stack:
-
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-      }
+starterController.controller("ExampleController", function($scope, $cordovaBarcodeScanner, $cordovaOauth, $http){
+  $scope.Scan = function(){
+    alert("Scanning !!!");
+    $cordovaBarcodeScanner.scan().then(function(imageData){
+        alert(imageData.text);
+    }, function(error){
+        alert("ERror while scanning : " + error);
+    });
+  };
+  $scope.LoginWithGoogle = function(){
+    
+    try{  
+      $cordovaOauth.google("75670157293-8bjfof6jaidmjub049e6424uv6ks5igu.apps.googleusercontent.com", 
+          ["email"]).then(function(result){
+        var accessToken = result.access_token;
+        $http.get("https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=" + accessToken).then(function(result, data){
+          alert("Welcome : " + result.data.email);
+        }, function(error){
+          alert("Error : " + JSON.stringify(error));
+        });
+      },function(error){
+        alert("ERROR GIAML : " + error);
+      });
     }
-  })
-
-  .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
-        }
-      }
-    })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
-    })
-
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
+    catch(Error){
+      alert("Eror : " + Error);
     }
-  });
-
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/login');
-
+  }
+  $scope.LoginWithFacebook = function(){
+    
+    $cordovaOauth.facebook("411152612427103", ["email"]).then(function(result) {
+            $http.get("https://graph.facebook.com/v2.2/me", { params: { access_token: result.access_token, fields: "email,id,name,gender,location,website,picture,relationship_status", format: "json" }}).then(function(result) {
+                var data = result.data;
+                alert("Welcome : " + data.email);
+            }, function(error) {
+                alert("There was a problem getting your profile.  Check the logs for details.");
+                console.log(error);
+            });
+        }, function(error) {
+            alert("There was a problem signing in!  See the console for logs");
+            console.log(error);
+        });
+  }
 });
